6a55596b35defc4bf978dd2f033eee11ca39c377
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-13 07:18:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ class BoardMember {
     this.training = training
   }
 
-    this.veto(){
+    veto(){
       return 'No, I must disagree'
     }
-    this.approve(){
+  approve(){
       return 'You can do that!'
     }
-    this.doCharity(){
+  doCharity(){
       return "I like to help people."
     }
 
-    this.releasePressStatement(){
+  releasePressStatement(){
       return "You will see great things from Scuber."
     }
 
-    this.sayHi()){
+  sayHi()){
       return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     }
   }
