b5ef9fcb4955196b8f23eea840e65a26be34f17f
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-13 07:15:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ class BoardMember {
       return "You will see great things from Scuber."
     }
 
-    BoardMember.prototype.sayHi = function(){
+    this.sayHi()){
       return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     }
